f0d5ea991a36126e22cc89738d6f73c3fd769f6f
cirelli20 <141993386+cirelli20@users.noreply.github.com>
2023-09-01 16:29:22 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
-
+function draw() {
+  background(220);
+}
 let angle = 0;
 let spacing = 5;
 let offset = 0;
@@ -10,7 +12,7 @@ function setup() {
 }
 
 function draw() {
-  translate(width / 2.5, height / 4.3);
+  translate(width / 2, height / 4);
   
   let x = cos(angle) * 350;
   let y = sin(angle * 2) * 200;
@@ -43,4 +45,6 @@ function draw() {
   textAlign(CENTER, TOP);
   fill(1, 2);
   noStroke ()
-  text("Los astros son rondas de niños jugando la Tierra a espiar...\n Los trigos son talles de niñas jugando a ondular..., a ondular... \n Los ríos son rondas de niños jugando a encontrarse en el mar... \n Las olas son rondas de niñas jugando la Tierra a abrazar.." ", width / 30, height / 5);
+  text("Los astros son rondas de niños jugando la Tierra a espiar...\n Los trigos son talles de niñas jugando a ondular... a ondular...\n Los ríos son rondas de niños jugando a encontrarse en el mar... \n Las olas son rondas de niñas jugando la Tierra a abrazar.. ", width / 12, height / 4);
+
+}
