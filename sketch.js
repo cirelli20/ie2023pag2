4b1b961a4809172fa797f87decb29dc3141d3e50
cirelli20 <antonia.gonzalez.t@mail.pucv.cl>
2023-09-22 02:46:42 -0300

    a

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-//primeros parametros del dibujo
 let angle = 0;
 let spacing = 5;
 let offset = 0;
@@ -6,34 +5,12 @@ let trail = [];
 let figuraEnMovimiento = true; // Variable para controlar si la figura se mueve
 
 function setup() {
-  createCanvas(1000, 2000); //area de trabajo
-  background(255, 255, 204); // Color amarillo claro
-
-  // Crear un enlace a una página externa
-  let enlaceInterno = createA('https://cirelli20.github.io/tarea2/', '- Tarea 2: cirelli20.github.io/tarea2/');
-  enlaceInterno.position(290, 660); //80ablecer la posición del enlace
-  enlaceInterno.style('font-size', '20px'); // Establecer el tamaño de fuente
-  enlaceInterno.style('color', 'ORANGE'); // Establecer el color del enlace
-  // Crear un enlace a una página externa
-    let enlaceInterno2 = createA('https://cirelli20.github.io/tarea-3/\n ', '- Tarea 3:cirelli20.github.io/tarea-3/ ');
-    enlaceInterno2.position(290, 700); //80ablecer la posición del enlace
-    enlaceInterno2.style('font-size', '20px'); // Establecer el tamaño de fuente
-    enlaceInterno2.style('color', 'ORANGE'); // Establecer el color del enlace
-  // Crear un enlace a la tarea 4
-   let enlaceInterno3 = createA('https://cirelli20.github.io/tarea-4/', '- Tarea 4:cirelli20.github.io/tarea-4/ ');
-   enlaceInterno3.position(290, 740); //80ablecer la posición del enlace
-   enlaceInterno3.style('font-size', '20px'); // Establecer el tamaño de fuente
-   enlaceInterno3.style('color', 'ORANGE'); // Establecer el color del enlace
-   
-  // Texto "ENCARGOS"
-  textSize(24);
-  fill(0);
-  text("ENCARGOS", 180, 530); // Ajusta la posición del texto
-  textFont(ITALIC)
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  translate(width / 2, height / 8);
+  
+  translate(width / 2, height / 50); // Centra el origen en el centro del lienzo
 
   if (figuraEnMovimiento) {
     let x = cos(angle) * 350;
@@ -43,7 +20,7 @@ function draw() {
     offset += 0.5;
 
     for (let i = 3; i < trail.length; i++) {
-      let alpha = map(i, 10, trail.length, 80, 400);
+      let alpha = map(i, 10, trail.length, 80, 40);
       stroke(10, alpha);
       if (i % 2 === 3) {
         strokeWeight(0.03);
@@ -61,24 +38,19 @@ function draw() {
 
     angle += 2;
   }
-
+translate(CENTER)
   // Texto y estilo...
   textSize(20);
   fill(1, 2);
   noStroke();
-  textStyle(ITALIC);
-  text("Los astros son rondas de niños \n jugando la Tierra a espiar...\n Los trigos son talles de niñas \n jugando a ondular... a ondular...\n   \n Los ríos son rondas de niños \n jugando a encontrarse en el mar... \n Las olas son rondas de niñas \n jugando la Tierra a abrazar..", width / 60, height / 7);
-}
-
-// Forma para que se detenga con un clic
-function mouseClicked() {
-  // Verificar si se hizo clic en la figura y cambiar el estado de movimiento
-  let distanciaAlCentro = dist(mouseX, mouseY, width / 2, height / 4);
-
-  if (distanciaAlCentro < 350 && figuraEnMovimiento) {
-    figuraEnMovimiento = false; // Detener la figura
-  } else {
-    figuraEnMovimiento = true; // Reanudar el movimiento
-  }
-  circle(30, 30, 20);
+  textFont ('Helvetica');
+  text("Los astros son rondas de niños \n jugando la Tierra a espiar...\n Los trigos son talles de niñas \n jugando a ondular... a ondular...\n   \n Los ríos son rondas de niños \n jugando a encontrarse en el mar... \n Las olas son rondas de niñas \n jugando la Tierra a abrazar..", -width / 4, -height / 2 + 30);
+ 
+  translate (CENTER);
+  textSize(30);
+  fill(1, 2);
+  noStroke();
+  textFont('Georgia');
+  text("ANTONIA GONZALEZ\n\nPortafolio Imagen Escrita ", -width / 4, -height / 2 + 30);
+  
 }
